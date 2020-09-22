1266bd6e8fe101c2574e5fb7246b2cf0cf057ec9
luiz <phellipe501@gmail.com>
2020-09-22 20:38:57 -0300

    Finalizando obrigatorios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import About from './About';
+import Header from './Header';
+import Favorites from './Favorites';
+import PokemonDetails from './PokemonDetails';
+import NotFound from './NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      <BrowserRouter>
+        <h1> Pokedex </h1>
+        <Header />
+          <Switch>
+            <Route path="/pokemons/:id" render={(props) => <PokemonDetails {...props} data={pokemons} />}/>
+            <Route path="/about" component={About}/>
+            <Route path="/favorites" component={Favorites}/>
+            <Route exact path="/" render={(props) => <Pokedex {...props} pokemons={pokemons} />}/>
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
